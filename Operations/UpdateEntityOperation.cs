133752b0ae62dc495a84ca040a67c0a818d21c16
Tom Studee <thomas.studee@gmail.com>
2016-03-16 17:11:11 -0500

    building out update entity operation...

---
+++ Operations/UpdateEntityOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Intacct.Entities;
+
+namespace Intacct.Operations
+{
+    public class UpdateEntityOperation<TEntity> : IntacctAuthenticatedOperationBase<TEntity> where TEntity : IntacctObject
+    {
+        public UpdateEntityOperation(IIntacctSession session, string functionName, string responseElementName, bool mayHaveEmptyResult = false) : base(session, functionName, responseElementName, mayHaveEmptyResult)
+        {
+        }
+
+        protected override XObject[] CreateFunctionContents()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IntacctOperationResult<TEntity> ProcessResponseData(XElement responseData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
