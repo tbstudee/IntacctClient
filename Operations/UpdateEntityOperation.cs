c131ed72c7ff88f05c9f49c12eb2d967e779172c
Tom B Studee <thomas.studee@gmail.com>
2016-03-22 09:35:53 -0500

    update entity op

--- Operations/UpdateEntityOperation.cs
+++ Operations/UpdateEntityOperation.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Intacct.Entities;
+using Intacct.Infrastructure;
 
 namespace Intacct.Operations
 {
     public class UpdateEntityOperation<TEntity> : IntacctAuthenticatedOperationBase<TEntity> where TEntity : IntacctObject
     {
-        public UpdateEntityOperation(IIntacctSession session, string functionName, string responseElementName, bool mayHaveEmptyResult = false) : base(session, functionName, responseElementName, mayHaveEmptyResult)
+        private readonly string _entityName;
+        private readonly string _entityId;
+        private readonly bool _isExternalKey;
+
+        public UpdateEntityOperation(IIntacctSession session, string entityId, string responseElementName, bool isExternalKey = false) : base(session, "update", responseElementName, mayHaveEmptyResult: true)
         {
+            if (entityId == null) throw new ArgumentNullException(nameof(entityId));
+            if (string.IsNullOrWhiteSpace(entityId)) throw new ArgumentException($"Argument {nameof(entityId)} may not be empty.", nameof(entityId));
+
+            _entityName = GetObjectName<TEntity>();
+            _entityId = entityId;
+            _isExternalKey = isExternalKey;
         }
 
         protected override XObject[] CreateFunctionContents()
@@ -21,7 +33,22 @@ namespace Intacct.Operations
 
         protected override IntacctOperationResult<TEntity> ProcessResponseData(XElement responseData)
         {
-            throw new NotImplementedException();
+            var entityElement = responseData.Element(_entityName);
+
+            var entity = (TEntity) Activator.CreateInstance(typeof (TEntity), entityElement);
+
+            return new IntacctOperationResult<TEntity>(entity);
+        }
+
+        private string GetObjectName<T>()
+        {
+            var attribute = typeof(T).GetTypeInfo().GetCustomAttribute<IntacctNameAttribute>();
+            if (attribute == null)
+            {
+                throw new Exception($"Unable to create \"update\" request for entity of type {typeof(T).Name} because it is missing the {nameof(IntacctNameAttribute)} attribute.");
+            }
+
+            return attribute.Name;
         }
     }
 }
