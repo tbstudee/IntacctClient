ab869b84b6f73be51ed02b273d782c09164738eb
Tom Studee <thomas.studee@gmail.com>
2016-08-09 17:25:51 -0500

    use ints for ids for simpler integration w/domain

--- Entities/IntacctLineItem.cs
+++ Entities/IntacctLineItem.cs
@@ -17,17 +17,17 @@ namespace Intacct.Entities
 	    }
 
         [IntacctName("glaccountno")]
-		public string AccountNumber { get; private set; }
+		public int AccountNumber { get; private set; }
         [IntacctName("accountlabel")]
 		public string AccountLabel { get; private set; }
         
 		public decimal Amount { get; set; }
 
 		public string Memo { get; set; }
-        public string LocationId { get; set; }
-        public string DepartmentId { get; set; }
+        public int LocationId { get; set; }
+        public int DepartmentId { get; set; }
 
-		public static IntacctLineItem CreateWithAccountNumber(string accountNumber, decimal amount)
+		public static IntacctLineItem CreateWithAccountNumber(int accountNumber, decimal amount)
 		{
 			return new IntacctLineItem(amount) { AccountNumber = accountNumber };
 		}
@@ -57,7 +57,7 @@ namespace Intacct.Entities
 
 		private XElement GetAccountXmlElement()
 		{
-			if (!string.IsNullOrWhiteSpace(AccountNumber)) return new XElement("glaccountno", AccountNumber);
+			if (!string.IsNullOrWhiteSpace(AccountNumber.ToString())) return new XElement("glaccountno", AccountNumber);
 			if (!string.IsNullOrWhiteSpace(AccountLabel)) return new XElement("accountlabel", AccountLabel);
 
 			throw new Exception("Unable to generate line item XML because neither account number or label are set.");
